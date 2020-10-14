43691856eaadcc47dc29e194df9b9bbdb568d44f
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-14 04:14:47 +0000

    [Add] 部分テンプレートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'users#show'
+  resources :users
+  resources :books
   get "/" => "homes#top"
-  get "homes/about" =>"homes#about" 
+  get "homes/about" =>"homes#about"
 end
