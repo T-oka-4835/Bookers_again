c664a9c96c54a9caafb7cc0e4094adaff86a1ac7
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-14 07:43:20 +0000

    [update] 部分テンプレートを各ページに実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def show
     @books = Book.all
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end 
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_path)
+    redirect_to user_path(@user.id)
+  end 
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end 
 end
