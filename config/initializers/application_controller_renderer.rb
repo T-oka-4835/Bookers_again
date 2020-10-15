cdbbc9fed6ba33441b14ceb3bca66f0658946b11
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-15 07:13:17 +0000

    [Add] UsersのEdit を追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6842f1b316d5413c77c67bf216029aa2393e1703b5c6947c7a8a5aa8ebf0d2da384c87f93df5ba2e39975dfc5020200214a0a4da884edb12392aaa0f0d4de79b'
